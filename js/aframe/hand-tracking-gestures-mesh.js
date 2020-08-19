2f6d2d71ae94bf80dc3e7bb1d8bcab983a0b2ada
Tomasz Morawski <tomasz.morawski@netguru.com>
2020-08-19 11:09:48 +0200

    Make keyboard work with duplicated characters

--- js/aframe/hand-tracking-gestures-mesh.js
+++ js/aframe/hand-tracking-gestures-mesh.js
@@ -1,52 +1,57 @@
-AFRAME.registerComponent('hand-tracking-gestures-mesh', {
+AFRAME.registerComponent("hand-tracking-gestures-mesh", {
+  dependencies: ["hand-tracking-mesh"],
 
-    dependencies: ['hand-tracking-mesh'],
+  init() {
+    this.handTracking = this.el.components["hand-tracking-mesh"];
+  },
 
-    init: function() {
-        this.handTracking = this.el.components['hand-tracking-mesh'];
+  tick() {
+    if (this.handTracking.jointsLoaded() && this.handTracking.mesh != null) {
+      const position = this.handTracking
+        .getJoints()
+        [XRHand.THUMB_PHALANX_TIP].position.clone();
 
-        this.isPinched = false;
-        var geometry = new THREE.BoxGeometry( 0.01, 0.01, 0.01 );
-        var material = new THREE.MeshBasicMaterial();
-        this.pinchHelper = new THREE.Mesh( geometry, material );
-        this.pinchHelper.visible = false;
-        this.el.sceneEl.object3D.add(this.pinchHelper)
-    },
+      const isIndexTouching = this.touchCheck(
+        this.handTracking,
+        XRHand.INDEX_PHALANX_TIP,
+        "INDEX"
+      );
+      const isMiddleTouching = this.touchCheck(
+        this.handTracking,
+        XRHand.MIDDLE_PHALANX_TIP,
+        "MIDDLE"
+      );
+      const isRingTouching = this.touchCheck(
+        this.handTracking,
+        XRHand.RING_PHALANX_TIP,
+        "RING"
+      );
+      const isLittleTouching = this.touchCheck(
+        this.handTracking,
+        XRHand.LITTLE_PHALANX_TIP,
+        "LITTLE"
+      );
 
-    tick: function() {
-        if (this.handTracking.jointsLoaded() && this.handTracking.mesh != null) {
-            const position = this.handTracking.getJoints()[XRHand.THUMB_PHALANX_TIP].position.clone();
-            this.pinchHelper.position.copy(position.add(new THREE.Vector3(0, 1.5, 0)));
-            if (this.pinchCheck(this.handTracking)) {
-                if (this.isPinched == false) {
-                    this.isPinched = true;
-                    this.el.emit('started-pinch', {
-                        joint: this.pinchHelper
-                    });
-                }
-            }
-            if (this.pinchReleaseCheck(this.handTracking)) {
-                if (this.isPinched == true) {
-                    this.isPinched = false;
-                    this.el.emit('ended-pinch')
-                }
-            }
-        }
-    },
+      const fingersState = [
+        isIndexTouching ? 1 : 0,
+        isMiddleTouching ? 1 : 0,
+        isRingTouching ? 1 : 0,
+        isLittleTouching ? 1 : 0,
+      ];
 
-    pinchCheck: function (hand) {
-        const joints = hand.getJoints();
-        const indexTip = joints[XRHand.INDEX_PHALANX_TIP];
-        const thumbTip = joints[XRHand.THUMB_PHALANX_TIP];
-        const distance = indexTip.position.distanceTo(thumbTip.position);
-        return distance < 0.01;
-    },
-    pinchReleaseCheck: function (hand) {
-        const joints = hand.getJoints();
-        const indexTip = joints[XRHand.INDEX_PHALANX_TIP];
-        const thumbTip = joints[XRHand.THUMB_PHALANX_TIP];
-        const distance = indexTip.position.distanceTo(thumbTip.position);
-        return distance > 0.03;
-    },
+      const state = {
+        hand: this.handTracking.data.hand,
+        fingersState,
+      };
 
+      this.el.emit("touchState", state);
+    }
+  },
+  touchCheck(hand, fingerId) {
+    const joints = hand.getJoints();
+    const thumbTip = joints[XRHand.THUMB_PHALANX_TIP];
+    const fingerTip = joints[fingerId];
+    const distance = fingerTip.position.distanceTo(thumbTip.position);
+    return distance < 0.015;
+  },
 });
